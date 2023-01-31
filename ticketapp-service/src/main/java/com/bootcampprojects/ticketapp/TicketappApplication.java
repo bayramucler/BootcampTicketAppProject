9eefc7e7d54288e26cc695a663850bc7d11650e5
bayram üçler <bayramucler@gmail.com>
2023-01-31 23:11:29 +0300

    eureka/gateway added

--- ticketapp-service/src/main/java/com/bootcampprojects/ticketapp/TicketappApplication.java
+++ ticketapp-service/src/main/java/com/bootcampprojects/ticketapp/TicketappApplication.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
 @ImportAutoConfiguration({ FeignAutoConfiguration.class })
+@EnableEurekaClient
 @OpenAPIDefinition(info = @Info(title = "TicketApp Service API", version = "1.0", description = "TicketApp Service API Information"))
 public class TicketappApplication {
 	public static void main(String[] args) {
