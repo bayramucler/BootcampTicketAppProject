e2a20eb136476d98e602e51070bc40faaf0afcb9
bayram üçler <bayramucler@gmail.com>
2023-01-31 23:07:44 +0300

    eureka server added

---
+++ ticketapp-eurake-server/src/main/java/ticketapp/TicketAppEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package ticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TicketAppEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAppEurekaServerApplication.class, args);
+	}
+
+}
